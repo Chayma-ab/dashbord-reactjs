94c2c228467318911739e30d2d6c1ef01589f811
Chayma-ab <abidallicha@gmail.com>
2023-05-23 16:07:22 +0100

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+
+// import { ECommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar,
+// Pie, Financial, ColorPiker, ColorMapping, Editor} from './pages';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+
 import './App.css';
-import styles from "./index.css"
+
 
 function App() {
+    const activeMenu = true ;
   return (
-    <h1 className='underline'>App - test</h1>
+    <BrowserRouter>
+    <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+            <TooltipComponent content="setting" position='Top'>
+                <button type="button" className='text-3wl p-3 hover:bg-light-gray text-white' style={{background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+                </button>     
+            </TooltipComponent>
+
+        </div>
+     { activeMenu ? (
+        <div className='w-72 fixed sidebar 
+        dar:bg-secindary-dark-bg
+        bg-white'>
+            <Sidebar />
+        </div>
+
+     ) : (
+        <div className='w-0
+        dark:bg-secondary-dark-bg'>
+         <Sidebar />
+         </div>
+     )}
+     <div  className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }>
+        <div className='fixed md:static
+        bg-main-bg dark:bg-main-dark-bg
+        navbar w-full'>
+            <Navbar />
+
+        </div>
+        <div>
+            <Routes>
+                {/* Dashboard  */}
+                <Route path="/" element={<Ecommerce />}  />
+                <Route path="/ecommerce" element={<Ecommerce />} />
+                {/* Pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+                {/* Apps */}
+                <Route path="/kunban" element= {<Kanban /> } />
+                <Route path="/editor" element= {<Editor /> } />
+                <Route path="/calendar" element= {<Calendar /> } />
+                <Route path="/color-picker" element= {<ColorPicker /> } /> 
+                {/* Charts */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/staked" element={<Stacked />} />
+            </Routes> 
+        </div>
+
+     </div>
+    </div>
+
+    </BrowserRouter>
   )
 }
 
